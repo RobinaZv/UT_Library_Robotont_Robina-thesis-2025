031b35a7990a0a718a4cdfb03efe24efc69ef2d1
Robina <robina.zvirgzdina@gmail.com>
2025-05-09 16:45:45 +0300

    more updates to the logic

--- robina_thesis/launch/example2.py
+++ robina_thesis/launch/example2.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Path to your map file
-    map_file = os.path.join(os.getcwd(), 's96.yaml')
+    map_file = os.path.join(os.getcwd(), 's101.yaml')
 
     return LaunchDescription([
 
